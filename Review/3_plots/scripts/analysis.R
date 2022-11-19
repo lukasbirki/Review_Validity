19e3e949c90779ca4b93188eecf8d078d5802808
lukasbirki <lukas.birkenmaier@uni-konstanz.de>
2022-11-20 00:04:50 +0100

    several plots

--- Review/3_plots/scripts/analysis.R
+++ Review/3_plots/scripts/analysis.R
@@ -1,87 +1,101 @@
 library(tidyverse)
 
-df <- readxl::read_xlsx("Review/3_plots/data/export_forms.xlsx") %>% 
+df <- readxl::read_xlsx("Review/3_plots/data/results_corrected.xlsx") %>% 
   filter(`E-Mail-Adresse` == "lukas.birkenmaier@outlook.de") %>% 
   mutate(id = paste(stringr::word(.$`This is the title of the Study`,1),
                     stringr::word(.$`These are the Authors`,1),sep = "_")) %>% 
   dplyr::rename(source = `What is the data source?`,
           language = `What is the data language?`,language_others = `If others: Name all languages in the data? If there is more than one language assessed, please separate languages by a comma (,)` ,
           method_type = `What Type(s) of method are initially applied?`,
-          method_name = `What is the name of the method? If there is more than one method applied, please separate methods by a comma (,)`,
-          target_construct = `Please name the target construct as described by the authors)\nNote: Please use the exact wording of the authors, such as \"affective polarization\", \"semantic complexity\".\nIf there is more than one construct studied, please separate constructs by a comma (,)`) %>% 
+          method_name = `What is the name of the method? If there is more than one method applied, please separate methods by a comma (,)`) %>% 
   drop_na(source)
 
+#No dublicates
+length(unique(df$id)) == nrow(df)
+
 #Creating variable for count of validation steps per publication
-names(df)
-df %>% select(id, ends_with("vtext")) %>%
-  mutate(dont_drop = case_when(
-    (`In what paragraphs is the step of validation / robustness checks / […] reported? 
-      Please copy the paragraph/description into the open field!...20` != NA) ~ "valid"
-  ))
-  pivot_longer(cols = 2:9, names_to = "step", values_to = "content") %>% 
-  drop_na(drop) %>% 
-  group_by(id) %>% mutate(no = row_number(),
-                          no_abs = max(no)) %>% 
-  ungroup() %>% 
-  distinct(id,no_abs) %>% 
-  ggplot(aes(x = no_abs))+
-    geom_bar()   
-  
-  
-ll$`In what paragraphs is the step of validation / robustness checks / […] reported? 
-Please copy the paragraph/description into the open field, so that the whole validation procedure is copied!`  
-ll$`In what paragraphs is the step of validation / robustness checks / […] reported? 
-Please copy the paragraph/description into the open field!...20`
 
-df$`In what paragraphs are issues of measurement validity / robustness checks / […] reported? Please copy them into the open field!...40`
+df_validation <- df %>% select(ends_with("_v1"))
+df$n_validation_steps <- rowSums(!is.na(df_validation))
 
-df %>% select(starts_with("In what"))
+#Changing into long format
 
-#Adding number of validation steps
-
-df %>% 
-  select(id, v_para)
+df %>%  
+  mutate(across(a_v1:f_v4, as.character)) %>% 
+  pivot_longer(cols = a_v1:f_v4)  %>%  
+  mutate(location = sub('.*(a|b|c|d|e|f).*', '\\1', name), 
+         name  = sub('_?(a|b|c|d|e|f)_?', '', name)) %>% 
+  pivot_wider() -> df_plot
 
 #Plots
 
 ## Data Source
-df %>% 
-  separate_rows(source,sep = ",") %>% 
+df_plot %>% 
+  distinct(id, source) %>% 
+  separate_rows(source,sep = ",") %>% mutate(source = trimws(source)) %>% 
   ggplot(aes(x=reorder(source, table(source)[source]))) +
   geom_bar() +
   coord_flip() +
-  xlab("") +
-  theme_bw()
+  labs(title = "Data Sources", x = "Data", y = "n") +
+  scale_colour_Publication()+
+  theme_Publication()
+
+ggsave("source.png")
 
 ## Language
 
-##Target Construct
-
-df %>% 
-  separate_rows(target_construct,sep = ",") %>% 
-  ggplot(aes(x=reorder(target_construct, table(target_construct)[target_construct]))) +
+df_plot %>% 
+  mutate(language_combined = case_when(
+    language == "English"~ language,
+    T ~ language_others)) %>% 
+  distinct(id, language_combined) %>% 
+  separate_rows(language_combined,sep = ",") %>% mutate(language_combined = trimws(language_combined)) %>% 
+  ggplot(aes(x=reorder(language_combined, table(language_combined)[language_combined]),fill = factor(language_combined))) +
   geom_bar() +
   coord_flip() +
-  xlab("") +
-  theme_bw()
+  labs(title = "Languages Analzed", x = "Language", y = "n", fill = "language") +
+  theme_Publication()
+
+ggsave("language.png")
 
 ## Method Type
-df %>% 
-  separate_rows(method_type,sep = ",") %>% 
-  ggplot(aes(x=reorder(method_type, table(method_type)[method_type]))) +
+df_plot %>% 
+  distinct(id, method_type) %>% 
+  mutate(method_short = case_when(
+    startsWith(as.character(method_type),"Super") ~ "Supervised",
+    startsWith(as.character(method_type),"Unsuper") ~ "Unsupervised",
+    startsWith(as.character(method_type),"Rule") ~ "Dictionary",
+                               T ~ "Others")) %>% 
+  ggplot(aes(x=reorder(method_short, table(method_short)[method_short]),fill = factor(method_short))) +
   geom_bar() +
   coord_flip() +
-  xlab("") +
-  theme_bw()
-
-## Method Name
-
-df %>% 
-  separate_rows(method_type,sep = ",") %>% 
-  ggplot(aes(x = method_type, y = target_construct)) +
-  geom_tile(color = "black") +
-  coord_fixed() +
-  theme(legend.position = "none")
+  labs(title = "Method Type", x = "Type", y = "n", fill = "Method") +
+  theme_Publication()+theme(axis.title.y = element_blank())
 
 ggsave("Tile_plot.png")
+
+#Count of validation steps
+
+df_plot %>% 
+  distinct(id, n_validation_steps) %>% 
+  ggplot(aes(x=n_validation_steps,fill = factor(n_validation_steps))) +
+  geom_bar()+
+  labs(title = "Number of Validation Steps", x = "Validation Steps", y = "n", fill = "Number of Validation Steps") +
+  theme_Publication() + theme(axis.title.y = element_blank())
+
+#Method and Validation
+
+df_plot %>% 
+  drop_na(v1) %>% 
+  mutate(method_short = case_when(
+    startsWith(as.character(method_type),"Super") ~ "Supervised",
+    startsWith(as.character(method_type),"Unsuper") ~ "Unsupervised",
+    startsWith(as.character(method_type),"Rule") ~ "Dictionary",
+    T ~ NA_character_)) %>% 
+  count(method_short,v3) %>% 
+  drop_na(method_short) %>% 
+ggplot(aes(method_short,v3, fill= n)) + 
+  geom_tile() +  labs(title = "Heatmap of CTAM Method and Validation Type", x = "Method", y = "Validation Type", fill = "n") +
+  theme_Publication() + scale_fill_gradient2()
+
 
