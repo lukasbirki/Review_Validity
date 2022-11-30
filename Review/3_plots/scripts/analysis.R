03cc9e1c92cf62bf1acd0788f95aa98d554c1972
lukasbirki <lukas.birkenmaier@uni-konstanz.de>
2022-11-30 21:54:08 +0100

    updates

--- Review/3_plots/scripts/analysis.R
+++ Review/3_plots/scripts/analysis.R
@@ -29,16 +29,20 @@ df %>%
   pivot_wider() -> df_plot
 
 #Plots
-
-
 df_plot %>% 
   distinct(id, source) %>% 
   separate_rows(source,sep = ",") %>% mutate(source = trimws(source)) %>% 
-  ggplot(aes(x=reorder(source, table(source)[source]), fill = source)) +
-  geom_bar() +
+  mutate(group = case_when(
+    startsWith(as.character(source),"Party") ~ "Party Politics",
+    startsWith(as.character(source),"Social") ~ "Social Media",
+    T ~ "Others")) %>% 
+  count(group, source, sort = T) %>% arrange(group,-n) %>% rowid_to_column() %>%
+  mutate(source = str_remove_all(source, c("Party Politics: ","Social Media: ")))  %>% 
+  ggplot(aes(x=reorder(source,-rowid), y = n, fill = group)) +
+  geom_bar(stat = "identity") +
   coord_flip() +
   labs(title = "Data Sources", x = "Data", y = "n") +
-  scale_fill_Publication()+
+  scale_fill_Publication(name = "Source",labels = c("Social Media", "Party Politics","Others"))+
   theme_Publication() -> barchart_source
 
 ggsave(plot =barchart_source ,filename = "Review/3_plots/output/barchart_source.png",width = 13, height = 6)
@@ -92,7 +96,7 @@ df_source_multiple %>%
   theme_Publication() +
   theme(axis.title.y = element_blank(),legend.position = "none")-> p3
 
-ggpubr::ggarrange(p1,p2,p3,labels = c("Social Media","Party Politics","Others")) -> souce_overview
+ggpubr::ggarrange(p1,p2,p3,labels = c("Social Media","Party Politics","Others"), nrow = 1) -> souce_overview
 
 ggsave(plot =souce_overview ,filename = "Review/3_plots/output/barchart_source_overview.png",width = 12, height = 8)
 
@@ -152,7 +156,85 @@ df_plot %>%
 
 ggsave("Review/3_plots/output/barchart_validation_count.png", width = 13, height = 6)
 
-#Method and Validation
+
+
+#Data and Code
+df_plot$`Is there a link to an Appendix/additional Materials/GitHub repository?`
+
+df_plot %>% 
+  rename(link = `Is there a link to an Appendix/additional Materials/GitHub repository?`) %>% 
+  select(id, source, link) %>% 
+  distinct(id,source,link) %>% 
+  ggplot(aes(x=link,fill = link)) +
+  geom_bar()+
+  labs(title = "Replication Materials available?",) +
+  theme_Publication() + scale_fill_Publication() + 
+  theme(axis.title.y = element_blank(), legend.position = "none",axis.title.x = element_blank())
+
+ggsave("Review/3_plots/output/replication.png", width = 8, height = 8)
+
+#Paper Plots ----
+
+## Decriptive ----
+
+### Source
+df_plot %>% 
+  distinct(id, source) %>% 
+  separate_rows(source,sep = ",") %>% mutate(source = trimws(source)) %>% 
+  mutate(group = case_when(
+    startsWith(as.character(source),"Party") ~ "Party Politics",
+    startsWith(as.character(source),"Social") ~ "Social Media",
+    T ~ "Others")) %>% 
+  count(group, source, sort = T) %>% arrange(group,-n) %>% rowid_to_column() %>%
+  mutate(source = str_remove_all(source, c("Party Politics: ","Social Media: ")))  %>% 
+  ggplot(aes(x=reorder(source,-rowid), y = n, fill = group)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(y = "n") +
+  scale_fill_Publication(name = "Source",labels = c("Social Media", "Party Politics","Others"))+
+  theme_Publication() +theme(axis.title.y = element_blank())-> barchart_source
+
+## Type
+df_plot %>% 
+  distinct(id, method_type) %>% 
+  mutate(method_short = case_when(
+    startsWith(as.character(method_type),"Super") ~ "Supervised",
+    startsWith(as.character(method_type),"Unsuper") ~ "Unsupervised",
+    startsWith(as.character(method_type),"Rule") ~ "Dictionary",
+    T ~ "Others")) %>% 
+  ggplot(aes(x=reorder(method_short, table(method_short)[method_short]),fill = factor(method_short))) +
+  geom_bar() +
+  labs(x = "Type", y = "n", fill = "Method") +
+  theme_Publication()+  scale_fill_Publication()+
+  theme(axis.title.y = element_blank(),legend.position = "none",axis.title.x = element_blank()) -> barchart_method
+
+### df_plot %>% 
+df_plot %>% mutate(language_combined = case_when(
+  language == "English"~ language,
+  T ~ language_others)) %>% 
+  distinct(id, language_combined) %>% 
+  separate_rows(language_combined,sep = ",") %>% mutate(language_combined = trimws(language_combined)) -> df_temp
+
+df_temp%>% 
+  count(language_combined) %>% left_join(df_temp, ., by = "language_combined") %>% 
+  mutate(language_combined = case_when(
+    (n > 1) ~ language_combined,
+    (n == 1) ~ "Others")) %>% 
+  ggplot(aes(x=reorder(language_combined, table(language_combined)[language_combined]),fill = factor(language_combined))) +
+  geom_bar() +
+  coord_flip() +
+  labs(x = "Language", y = "n", fill = "Language") +
+  theme_Publication()+
+  scale_fill_Publication()+
+  theme(axis.title.y = element_blank(),legend.position = "none") -> barchart_language
+
+ggpubr::ggarrange(barchart_source,
+                  ggpubr::ggarrange(barchart_language,barchart_method,labels = c("Data Language","Method Type")),labels = c("Data Source",""),nrow = 2) -> souce_overview
+
+
+ggsave(plot =souce_overview ,filename = "Review/3_plots/output/barchart_source.png",width = 10, height = 10)
+
+## Method and Validation ----
 
 df_plot %>% 
   drop_na(v1) %>% 
@@ -163,18 +245,18 @@ df_plot %>%
     T ~ NA_character_)) %>% 
   count(method_short,v3) %>% 
   drop_na(method_short) %>% 
- ggplot(aes(method_short,v3, fill= n)) + 
+  ggplot(aes(method_short,v3, fill= n)) + 
   geom_tile() +  labs(title = "Heatmap of CTAM Method and Validation Type", fill = "n") +
   theme_Publication() + scale_fill_gradient2() +
   scale_x_discrete(labels = label_wrap(15))+
   scale_y_discrete(labels = label_wrap(20))+
   theme(legend.direction = "vertical",legend.position = "right",
         legend.key.size= unit(0.5, "cm"),
-    axis.title.y = element_blank())
+        axis.title.y = element_blank())
 
 ggsave("Review/3_plots/output/heatmap_methodxtype.png", width = 10, height = 8)
 
-#Method and Count Validation Steps
+## Method and Count Validation Steps ----
 
 df_plot %>% 
   distinct(id, n_validation_steps,method_type) %>% 
@@ -185,12 +267,14 @@ df_plot %>%
     T ~ NA_character_)) %>% 
   count(method_short,n_validation_steps) %>% 
   drop_na(method_short) %>% 
-  ggplot(aes(method_short,n_validation_steps, fill= n)) + 
+  ggplot(aes(method_short,as.character(n_validation_steps), fill= n)) + 
   geom_tile() +  labs(title = "Heatmap of CTAM Method and Validation Type", fill = "n") +
   theme_Publication() + scale_fill_gradient2() +
   scale_x_discrete(labels = label_wrap(15))+
   scale_y_discrete(labels = label_wrap(20))+
   theme(legend.direction = "vertical",legend.position = "right",
         legend.key.size= unit(0.5, "cm"),
-        axis.title.y = element_blank())
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),)
 
+ggsave("Review/3_plots/output/heatmap_methodxn_validation.png", width = 10, height = 8)
